7e5fad94f1f1fc0269fb8e492c3ea16f5880d244
Sonali Sinha <9399sonalisinha@gmail.com>
2024-07-26 12:46:22 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,7 +3,7 @@ const getTotalIsles = function (grid) {
 
   const rows = grid.length;
   const cols = grid[0].length;
-  const visited = Array.from({length : rows} , ()=> Array(cols).file(false));
+  const visited = Array.from({length : rows} , ()=> Array(cols).fill(false));
   
 
   const directions =[
