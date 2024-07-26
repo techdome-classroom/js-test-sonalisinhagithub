fd38ccac86621dfdeec142a00ab1283db1fa30a7
Sonali Sinha <9399sonalisinha@gmail.com>
2024-07-26 12:31:50 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -19,7 +19,8 @@ const getTotalIsles = function (grid) {
   const dfs = (r,c) => {
     const stack =[[r,c]];
     while (stack.length){
-      const [curRow , ]
+      const [curRow , curCol] = stack.pop();
+      if(!isValid (curRow , curCol))
     }
   }
   
